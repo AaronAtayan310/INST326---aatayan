c5a411429dd48f18d004e6635f5ba95780f21321
AaronAtayan310 <aatayan@terpmail.umd.edu>
2025-09-05 19:14:37 -0400

    Update numberguess.py

--- Library/Personal/numberguess.py
+++ Library/Personal/numberguess.py
@@ -4,19 +4,19 @@ n1 = int(input("Enter a lower bound number: "))
 n2 = int(input("Enter an upper bound number: "))
 answer = random.randint(n1, n2)
 
-guess = int(input("Enter your guess of what the number generated is: "))
-
 i = 0
-while i < 100:
-    i += 1
+while True:
     guess = int(input("Enter your guess of what the number generated is: "))
+    i += 1
+
     if guess < answer:
         print("Guess is lower than Answer")
-        continue
-    if guess > answer:
-        print("Guess is higher than Answer")
-        continue
-    if guess == answer:
-        print("Correct! The generated number was: " + answer)
-        print("Total guesses: " + i)
+    elif guess > answer:
+        print("Guess is greater than Answer")
+    else:
+        if i == 1:
+            print("Nice! You got it in one try!!")
+        else:
+            print("Correct! The generated number was: " + str(guess))
+            print("Total guesses: " + str(i))
         break
