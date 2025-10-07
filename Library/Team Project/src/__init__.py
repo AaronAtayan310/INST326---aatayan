a4a2419fca66451cc55daef838c2c6d1cf5b2507
AaronAtayan310 <aatayan@terpmail.umd.edu>
2025-10-06 22:06:24 -0400

    Update __init__.py

---
+++ Library/Team Project/src/__init__.py
@@ -0,0 +1,11 @@
+'''
+Crime Data Pipeline Package
+   
+A framework for processing and analyzing crime datasets.
+'''
+
+
+__version__ = "0.1.0"
+__authors__ = "Aaron Atayan, Vyvyan Mai, Sean McLean"
+
+#complete later
